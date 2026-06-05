fac307f4881d0dba5ae509484260489d9743a592
Jacob <59778322+Jacob-Lin-04@users.noreply.github.com>
2026-06-04 21:11:18 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,20 @@
+import collections
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #dict to map keys : freq
+        freq = dict()
+        
+        for num in nums:
+            #0 if key doesnt exist yet
+            freq[num] = freq.get(num, 0) + 1
+
+        # finds k number of keys with largest values 
+        top_k = heapq.nlargest(k, freq.keys(), key=lambda x: freq[x])
+
+        #return topk keys
+        return top_k
+        
+
+        
