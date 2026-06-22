02cf0f7c09af235219c8f04f4ebb8438276f9b38
Jacob <59778322+Jacob-Lin-04@users.noreply.github.com>
2026-06-21 18:38:17 -0700

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # search through every row
+        for i, row in enumerate(matrix):
+            left = 0 
+            right = len(matrix[i]) - 1
+            
+            # binary search through each row since it is sorted
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                # look at upper half
+                if target > matrix[i][mid]:
+                    left = mid + 1
+                # look at lower half
+                elif target < matrix[i][mid]:
+                    right = mid - 1 
+                else:
+                    return True
+
+        return False
