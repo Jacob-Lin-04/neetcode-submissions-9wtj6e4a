be11afafcf7e4e8f3dd23e121ade205c4d4fbaca
Jacob <59778322+Jacob-Lin-04@users.noreply.github.com>
2026-09-08 14:03:59 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #empty dict
+        e = dict()
+        #search through list
+        for i, num in enumerate(nums):
+            #value needed to achive target
+            complement = target - num
+            
+            #check if value exists as a key
+            if complement in e:
+                #if found return index for complement and current index
+                return [e[complement], i]
+            else:
+                #store current value and its index for future lookup
+                e[num] = i
+        
